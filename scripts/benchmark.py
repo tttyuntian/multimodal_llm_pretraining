205cfe036cd61136c157b58393581a4d1b4809e0
apoorvkh <mail@apoorvkh.com>
2024-11-02 18:28:37 -0400

    scripts/benchmark.py argument validation

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,12 +1,34 @@
+import math
 import signal
 import sys
 from typing import Literal
 
 import tyro
 from experiments import Sweep
-from experiments.config import GpuT
 from experiments.training_time_empirical_sweep import TrainingTimeEmpiricalSweep
-from src.models import ModelT
+from src.gpus import GpuT, ampere_or_newer_gpu
+from src.models import ModelT, get_model_class
+
+
+def validate_arguments(
+    num_nodes: int,
+    gpus_per_node: int,
+    gpu_type: GpuT,
+    model: ModelT,
+):
+    model_class = get_model_class(model)
+
+    num_gpus = num_nodes * gpus_per_node
+
+    assert (
+        model_class.batch_size % num_gpus == 0
+    ), f"model batch size ({model_class.batch_size}) should be evenly divisible by total GPUs ({num_gpus})"
+    assert math.log2(
+        model_class.batch_size // num_gpus
+    ).is_integer(), f"batch size per gpu ({model_class.batch_size // num_gpus}) should be power of 2"
+
+    if model_class.mixed_precision == "bf16":
+        assert ampere_or_newer_gpu(gpu_type), "GPU must be ampere or newer to use mixed precision with bf16"
 
 
 def run_benchmark(
@@ -18,6 +40,8 @@ def run_benchmark(
     cmd: Literal["run", "count", "print-incomplete", "print-results"] = "run",
     slurm: bool = False,
 ) -> None:
+    validate_arguments(num_nodes, gpus_per_node, gpu_type, model)
+
     # Naive settings
     free_lunch = [False]
     activation_checkpointing = [False]
